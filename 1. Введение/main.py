f68f319c0a302ba668115f0939ddd1f4aeca9c74
Андрей Боев <a.boev.it@gmail.com>
2024-12-04 11:51:40 +0300

    1. Введение

---
+++ 1. Введение/main.py
@@ -0,0 +1,14 @@
+print('Text 1', 'Text 2', 'Text 3', end=' ')
+print('Text 4', 'Text 5', 'Text 6')
+
+# Text 1 Text 2 Text 3 Text 4 Text 5 Text 6
+
+print('Text 1', 'Text 2', 'Text 3', 'Text 4', 'Text 5', 'Text 6', sep='\n')
+"""
+Text 1
+Text 2
+Text 3
+Text 4
+Text 5
+Text 6
+"""
